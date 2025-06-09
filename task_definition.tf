b9a2960e2d71e8ed2d1d60721d21a232da5b2293
willdavies0 <42768478+willdavies0@users.noreply.github.com>
2025-06-09 11:22:01 +0100

    FEAT: add unique task definition with name prefix and random suffix (#9)
    * feat: add unique task definition with name prefix and random suffix
    
    * moving suffix to end

--- task_definition.tf
+++ task_definition.tf
@@ -22,10 +22,53 @@ locals {
   ])
 }
 
-resource "aws_ecs_task_definition" "task_definition" {
+resource "aws_ecs_task_definition" "task_definition_old" {
   for_each = { for task in local.task_defs : "${task.integration}_${task.python_version}" => task }
 
   family                   = "${each.key}_PIP"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = each.value.cpu
+  memory                   = each.value.memory
+
+  execution_role_arn = aws_iam_role.compute_execute_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name        = each.value.integration
+      image       = "${local.ecr_account_mapping[var.orchestra_aws_account_id]}.dkr.ecr.${var.region}.amazonaws.com/${each.value.image}:${each.value.python_version}_PIP-${var.image_tags[each.value.integration]}"
+      cpu         = each.value.cpu
+      memory      = each.value.memory
+      environment = var.task_env_vars,
+      secrets     = var.task_secrets,
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.compute_log_group.name
+          "awslogs-region"        = var.region
+          "awslogs-stream-prefix" = each.value.integration
+        }
+      }
+    }
+  ])
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      "NewLogGroup" = "true"
+    }
+  )
+}
+
+resource "aws_ecs_task_definition" "task_definition" {
+  for_each = { for task in local.task_defs : "${task.integration}_${task.python_version}" => task }
+
+  family                   = "${var.name_prefix}_${each.key}_PIP_${random_id.random_suffix.hex}"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = each.value.cpu
